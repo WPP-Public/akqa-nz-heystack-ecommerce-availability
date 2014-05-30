6f0ce7c6b74beeb0f4355a0b2d4e38dd0dcf2d32
Cam Spiers <camspiers@gmail.com>
2014-05-30 13:43:03 +1200

    Fix composer autoloading

--- src/SilverStripe/AvailabilityExtension.php
+++ src/SilverStripe/AvailabilityExtension.php
@@ -2,14 +2,17 @@
 
 namespace Heystack\Availability\SilverStripe;
 
-use Heystack\Ecommerce\Locale\Traits\HasLocaleServiceTrait;
+use Controller;
+use Heystack\Ecommerce\Zone\Traits\HasZoneServiceTrait;
+use Versioned;
+use LeftAndMain;
 
 /**
  * @package Heystack\Availability\SilverStripe
  */
 class AvailabilityExtension extends \DataExtension
 {
-    use HasLocaleServiceTrait;
+    use HasZoneServiceTrait;
 
     /**
      * @var array
@@ -34,5 +37,32 @@ class AvailabilityExtension extends \DataExtension
             'Root.Main',
             new \CheckboxField('NotAvailable', 'Currently Unavailable')
         );
+    }
+
+    /**
+     * Change the SQL to ensure that products that aren't available aren't selected
+     * @param \SQLQuery $query
+     */
+    public function augmentSQL(\SQLQuery &$query)
+    {
+        $controller = Controller::curr();
+
+        if ($controller && !$controller instanceof LeftAndMain) {
+
+            $productTable = get_class($this->owner);
+            $siteTreeTableMatch = sprintf(
+                'SiteTree%s.ID',
+                Versioned::current_stage() === 'Live' ? '_Live' : ''
+            );
+
+            // left join on ID and select on availability
+            $query->addLeftJoin("{$productTable}_AvailabilityZones", "{$productTable}_AvailabilityZones.{$productTable}ID = $siteTreeTableMatch");
+            $query->addLeftJoin( "Zone", "{$productTable}_AvailabilityZones.ZoneID = Zone.ID");
+            $query->addWhere(sprintf(
+                "Zone.Name = '%s'",
+                \Convert::raw2sql($this->zoneService->getActiveZone()->getName())
+            ));
+            $query->addWhere("NotAvailable != 1");
+        }
     }
 } 
