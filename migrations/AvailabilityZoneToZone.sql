8323fa980025c263578b4264f643042b423ac795
Cam Spiers <camspiers@gmail.com>
2014-06-04 12:45:55 +1200

    Remove migrations. They are now in coredb

--- migrations/AvailabilityZoneToZone.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `Heystack\DB\Zone` (ID, Created, LastEdited, Name)
-  SELECT ID, Created, LastEdited, Name FROM AvailabilityZone;
-
-INSERT INTO `Heystack\DB\Country` (`ID`, `Created`, `LastEdited`, `Name`, `CountryCode`, `IsDefault`, `ZoneID`)
-  SELECT `ID`, `Created`, `LastEdited`, `Name`, `CountryCode`, `IsDefault`, `ZoneID` FROM AvailabilityCountry;
